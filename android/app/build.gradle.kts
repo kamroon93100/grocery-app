6bf7d11d23cd5eacdb6553fe6525792dcb5ed14a
Karman Singh <kgurdev25@gmail.com>
2026-06-27 14:13:47 +0530

    Add proper APK signing config

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,4 +1,12 @@
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import java.util.Properties
+import java.io.FileInputStream
+
+val keystoreProperties = Properties()
+val keystorePropertiesFile = rootProject.file("key.properties")
+if (keystorePropertiesFile.exists()) {
+    keystoreProperties.load(FileInputStream(keystorePropertiesFile))
+}
 
 plugins {
     id("com.android.application")
@@ -7,7 +15,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.grocery_local"
+    namespace = "com.kohli.store"
     compileSdk = 36
     ndkVersion = flutter.ndkVersion
 
@@ -18,17 +26,27 @@ android {
     }
 
     defaultConfig {
-        applicationId = "com.example.grocery_local"
+        applicationId = "com.kohli.store"
         minSdk = flutter.minSdkVersion
         targetSdk = 36
-        versionCode = flutter.versionCode
-        versionName = flutter.versionName
+        versionCode = 1
+        versionName = "1.0.0"
         multiDexEnabled = true
+    }
+
+    signingConfigs {
+        create("release") {
+            keyAlias      = keystoreProperties["keyAlias"] as String?
+            keyPassword   = keystoreProperties["keyPassword"] as String?
+            storeFile     = if (keystoreProperties["storeFile"] != null)
+                            file(keystoreProperties["storeFile"] as String) else null
+            storePassword = keystoreProperties["storePassword"] as String?
+        }
     }
 
     buildTypes {
         release {
-            signingConfig = signingConfigs.getByName("debug")
+            signingConfig = signingConfigs.getByName("release")
             isMinifyEnabled = false
             isShrinkResources = false
         }
